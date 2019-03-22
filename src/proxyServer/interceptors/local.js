f8dd150060960b158f73c1757253f0a8ca0015e7
tjbo <tomoconnor@helpscout.com>
2019-03-22 16:52:54 -0700

    Remove unused FS

--- src/proxyServer/interceptors/local.js
+++ src/proxyServer/interceptors/local.js
@@ -1,5 +1,3 @@
-const fs = require('fs')
-
 module.exports = function({ localMappings, logIntercept, proxyServer }) {
   function addInterceptor(mapping) {
     const [targetUrl, localLocation] = mapping
