a8822a8e2853b9951e2f5d32c8eb1185f1d9e225
tjbo <tomoconnor@helpscout.com>
2019-08-12 20:42:29 -0500

    Add getLocalFile to webpack server

--- src/webpack/server.js
+++ src/webpack/server.js
@@ -1,4 +1,5 @@
 const CONFIG = require('../constants/config')
+const getLocalFile = require('../utils/getLocalFile')
 const https = require('https')
 const state = require('../proxyServer/state')
 const fs = require('fs')
@@ -22,20 +23,22 @@ const init = function() {
 
   https
     .createServer(options, function(request, response) {
-      fs.readFile(webpackCompilerLocalOutputPath + request.url, function(
-        err,
-        data,
-      ) {
-        if (!err) {
-          response.writeHead(203, { 'proxy-pack-source': 'webpack server' })
-          response.write(data || '')
+      const path = `${webpackCompilerLocalOutputPath}${request.url}`
+
+      getLocalFile(path)
+        .then(file => {
+          response.writeHead(203, {
+            'proxy-pack-webpack-server': path,
+            'content-type': file.contentType,
+          })
+          response.write(file.buffer || '')
           response.end()
-        } else {
+        })
+        .catch(err => {
           response.writeHead(500)
           response.write(err)
           response.end()
-        }
-      })
+        })
     })
     .listen(CONFIG.LOCAL_WEBPACK_SERVER.PORT)
 }
